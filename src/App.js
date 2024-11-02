bf5e8e44380ec9ef092ca895f17acc001b8d4423
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-02 11:21:29 +0530

    import udpates -4

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-// import Sample from './components/Sample';
-import LandingPage from './pages/LandingPage.jsx';
-import ResumePage from './pages/ResumePage.jsx';
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import ContactPage from './pages/ContactPage.jsx';
-import { color } from 'framer-motion';
-import Sample from './Sample.js';
 
 function App() {
   return (
-    <>
-      {/* <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={<LandingPage />} />
-          <Route path="/resume" element={<ResumePage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </Router>
-      <LandingPage/>
-      <div style={{ color: 'black' }}>
-        Version 1.0
-      </div>
-      <div style={{ color: 'white' }}>
-        Version 1.0
-      </div> */}
-      <Sample/>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-
-
 export default App;
+
