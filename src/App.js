6243d98226540aad9084b6d9ba78c2235d854a99
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-02 11:06:20 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import LandingPage from './pages/LandingPage';
 import ResumePage from './pages/ResumePage';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import ContactPage from './pages/ContactPage';
+import { color } from 'framer-motion';
 
 function App() {
   return (
+    <>
       <Router>
         <Routes>
-          <Route path="/" element={<LandingPage/>} />
-          <Route path="/home" element={<LandingPage/>} />
-          <Route path="/resume" element={<ResumePage/>} />
-          <Route path="/contact" element={<ContactPage/>} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </Router>
+      <div style={{ color: 'black' }}>
+        Version 1.0
+      </div>
+      <div style={{ color: 'white' }}>
+        Version 1.0
+      </div>
+    </>
   );
 }
 
