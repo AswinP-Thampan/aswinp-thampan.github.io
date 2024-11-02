5770436f64f231e244c568c28d46d2fd9903fe0c
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-02 11:11:58 +0530

    import udpates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 // import Sample from './components/Sample';
-import LandingPage from './pages/LandingPage';
-import ResumePage from './pages/ResumePage';
+import LandingPage from './pages/LandingPage.jsx';
+import ResumePage from './pages/ResumePage.jsx';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import ContactPage from './pages/ContactPage';
+import ContactPage from './pages/ContactPage.jsx';
 import { color } from 'framer-motion';
 
 function App() {
