48fcb123683c691734236a4de5ba6c31bc6fa8ae
AswinP-Thampan <aswinp.thampan@gmail.com>
2024-11-03 10:02:28 +0530

    mobile view -6

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,7 +6,7 @@ export default function Resume() {
     return (
         <div className='scroll-container' style={{ height: 'calc(100vh - 60px)', overflowY: 'scroll' }}>
             <div>
-                <div><h2>Resume</h2></div>
+                <div><h2>RESUME</h2></div>
                 <div className='main-div'>
                     <div>Work Experience</div>
                         {workexp.map((work) =>
@@ -29,11 +29,11 @@ const ProjectCards = ({ project }) => {
     return (
         <div className="cards">
             <div>
-                <div style={{ display: 'flex', fontSize: '14px' }}>{project.date} </div>
+                <div style={{ display: 'flex', fontSize: '14px' , color: "#8c8c8c" }}>{project.date} </div>
                 <div style={{ display: 'flex', fontSize: '20px' }}>{project.name}</div>
                 <hr style={{ marginLeft: "0px", opacity: '0.5' }} />
             </div>
-            <div style={{ fontSize: '18px' }}>{project.content}</div>
+            <div className='cards-font'>{project.content}</div>
             <div style={{ display: 'flex', gap: '10px', marginTop: '10px', flexWrap: 'wrap' }}>
                 {project.stack.map((item, index) =>
                     <div className='pills-stack' key={index}>{item}</div>
@@ -49,12 +49,12 @@ const WorKExpCards = ({ work }) => {
     return (
         <div className="cards">
             <div>
-                <div style={{ display: 'flex', fontSize: '14px' }}>{work.startDate} - {work.endDate} </div>
+                <div style={{ display: 'flex', fontSize: '14px' , color: "#8c8c8c"  }}>{work.startDate} - {work.endDate} </div>
                 <div style={{ display: 'flex', fontSize: '20px' }}>{work.role} · {work.company} ({work.team})</div>
-                <div style={{ display: 'flex', fontSize: '18px' }}>{work.location} </div>
+                <div style={{ display: 'flex', fontSize: '18px' , color: "#8c8c8c" }}>{work.location} </div>
                 <hr style={{ marginLeft: "0px", opacity: '0.5' }} />
             </div>
-            <div style={{ fontSize: '18px' }}>{work.content}</div>
+            <div className='cards-font'>{work.content}</div>
             <div style={{ display: 'flex', gap: '10px', marginTop: '10px', flexWrap: 'wrap' }}>
                 {work.stack.map((item, index) =>
                     <div className='pills-stack' key={index}>{item.name}</div>
